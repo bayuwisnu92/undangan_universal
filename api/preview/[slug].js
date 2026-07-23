484403abe2b3731918c8c1cfdf2e6d166cc05e15
bayuwisnu92 <bayuwisnu9294@gmail.com>
2026-07-23 07:19:12 +0700

    Test preview function

--- api/preview/[slug].js
+++ api/preview/[slug].js
@@ -1,6 +1,28 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    slug: req.query.slug
-  });
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+);
+
+export default async function handler(req, res) {
+
+    const { slug } = req.query;
+
+    console.log("Slug:", slug);
+
+    const { data, error } = await supabase
+        .from("weddings")
+        .select("*")
+        .eq("slug", slug);
+
+    console.log("DATA:", data);
+    console.log("ERROR:", error);
+
+    return res.json({
+        slug,
+        data,
+        error
+    });
+
 }
